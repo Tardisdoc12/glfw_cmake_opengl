e580f5f9695233868e6c797e11a9d75107236b97
Tardisdoc12 <jean.anquetil@livecampus.tech>
2024-12-28 22:04:30 +0100

    on prépare le GraphicEngine

---
+++ src/file_h/graphics/GraphicEngine.h
@@ -0,0 +1,34 @@
+//------------------------------------------------------------------------------
+// Name of this file : GraphicEngine.h
+// Author : Jean Anquetil
+// Date : 28/12/2024
+//------------------------------------------------------------------------------
+
+#pragma once
+#include <iostream>
+#include <map>
+#include "Framebuffer.h"
+
+//------------------------------------------------------------------------------
+
+class GraphicEngine
+{
+public:
+    GraphicEngine();
+
+    ~GraphicEngine();
+
+    void update();
+
+    void render();
+
+private:
+    std::map<int, Framebuffer> _framebuffers;
+    //std::vector<Shader> _shaders;
+    //std::vector<Texture> _textures;
+
+};
+
+//------------------------------------------------------------------------------
+// END OF FILE
+//------------------------------------------------------------------------------
