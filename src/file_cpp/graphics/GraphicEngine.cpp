c53d63b089cc4a50e09bff601095c353009fd6d9
Tardisdoc12 <jean.anquetil@livecampus.tech>
2025-01-08 17:44:10 +0100

    on utilise les variables globales

--- src/file_cpp/graphics/GraphicEngine.cpp
+++ src/file_cpp/graphics/GraphicEngine.cpp
@@ -7,14 +7,22 @@
 #include "../../file_h/graphics/GraphicEngine.h"
 #include "../../file_h/globals/variables.h"
 
+
+using namespace globals::graphic;
+
 //------------------------------------------------------------------------------
 
 GraphicEngine::GraphicEngine()
 {
+    pipelinesCor["default"] = 0;
+    pipelinesCor["screen"] = 1;
+
+    frameBuffersCor["toBlit"] = 0;
+
     auto shaderProgramdefault = std::make_shared<Shader>("shaders/default.vert", "shaders/default.frag");
     auto shaderProgramscreen = std::make_shared<Shader>("shaders/screen.vert", "shaders/default.frag");
-    _shaders[0] = std::move(shaderProgramdefault);
-    _shaders[1] = std::move(shaderProgramscreen);
+    _shaders[pipelinesCor["default"]] = std::move(shaderProgramdefault);
+    _shaders[pipelinesCor["screen"]] = std::move(shaderProgramscreen);
 
     auto cat_texture = std::make_shared<Texture>("images/cat.jpg");
     _textures[globals::entitiesCor["cat"]] = std::move(cat_texture);
@@ -25,33 +33,33 @@ GraphicEngine::GraphicEngine()
     auto screenData = ScreenData();
     _meshes[globals::entitiesCor["screen"]].push_back(std::make_shared<Mesh<ScreenData>>());
 
-    _framebuffers[0] = std::make_shared<Framebuffer>();
-    _framebuffers[0]->attachTexture(800, 600);
-    _framebuffers[0]->attachRenderbuffer(800, 600);
+    _framebuffers[frameBuffersCor["toBlit"]] = std::make_shared<Framebuffer>();
+    _framebuffers[frameBuffersCor["toBlit"]]->attachTexture(800, 600);
+    _framebuffers[frameBuffersCor["toBlit"]]->attachRenderbuffer(800, 600);
 
-    if (!_framebuffers[0]->isComplete())
+    if (!_framebuffers[frameBuffersCor["toBlit"]]->isComplete())
     {
         std::cerr << "Framebuffer is not complete" << std::endl;
     }
-    _shaders[1]->use();
-    _shaders[1]->set_single_form("Texture", "texture1");
-    glUniform1i(_shaders[1]->fetch_single_uniform("Texture"), 0);
+    _shaders[pipelinesCor["screen"]]->use();
+    _shaders[pipelinesCor["screen"]]->set_single_form("Texture", "texture1");
+    glUniform1i(_shaders[pipelinesCor["screen"]]->fetch_single_uniform("Texture"), 0);
 
-    _shaders[0]->use();
-    _shaders[0]->set_single_form("Texture", "texture1");
-    glUniform1i(_shaders[0]->fetch_single_uniform("Texture"), 0);
+    _shaders[pipelinesCor["default"]]->use();
+    _shaders[pipelinesCor["default"]]->set_single_form("Texture", "texture1");
+    glUniform1i(_shaders[pipelinesCor["default"]]->fetch_single_uniform("Texture"), 0);
     
     _modelMatrix = get_model_matrix(glm::vec3(0.0f, 0.0f, -1.f), glm::vec3(0.0f, 45.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
     _viewMatrix = get_view_matrix(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     _projectionMatrix = get_projection_matrix(45.0f, 800.0f / 600.0f, 0.1f, 100.0f);
     
-    _shaders[0]->set_single_form("View", "ViewMatrix");
-    _shaders[0]->set_single_form("Projection", "ProjectionMatrix");
-    _shaders[0]->set_single_form("Model", "ModelMatrix");
+    _shaders[pipelinesCor["default"]]->set_single_form("View", "ViewMatrix");
+    _shaders[pipelinesCor["default"]]->set_single_form("Projection", "ProjectionMatrix");
+    _shaders[pipelinesCor["default"]]->set_single_form("Model", "ModelMatrix");
     
-    glUniformMatrix4fv(_shaders[0]->fetch_single_uniform("View"), 1, GL_FALSE, glm::value_ptr(_viewMatrix));
-    glUniformMatrix4fv(_shaders[0]->fetch_single_uniform("Projection"), 1, GL_FALSE, glm::value_ptr(_projectionMatrix));
-    glUniformMatrix4fv(_shaders[0]->fetch_single_uniform("Model"), 1, GL_FALSE, glm::value_ptr(_modelMatrix));
+    glUniformMatrix4fv(_shaders[pipelinesCor["default"]]->fetch_single_uniform("View"), 1, GL_FALSE, glm::value_ptr(_viewMatrix));
+    glUniformMatrix4fv(_shaders[pipelinesCor["default"]]->fetch_single_uniform("Projection"), 1, GL_FALSE, glm::value_ptr(_projectionMatrix));
+    glUniformMatrix4fv(_shaders[pipelinesCor["default"]]->fetch_single_uniform("Model"), 1, GL_FALSE, glm::value_ptr(_modelMatrix));
 
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_STENCIL_TEST);
@@ -68,8 +76,8 @@ GraphicEngine::~GraphicEngine() = default;
 void GraphicEngine::update()
 {
     _modelMatrix = update_model_matrix(_modelMatrix, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.5f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-    _shaders[0]->use();
-    glUniformMatrix4fv(_shaders[0]->fetch_single_uniform("Model"), 1, GL_FALSE, glm::value_ptr(_modelMatrix));
+    _shaders[pipelinesCor["default"]]->use();
+    glUniformMatrix4fv(_shaders[pipelinesCor["default"]]->fetch_single_uniform("Model"), 1, GL_FALSE, glm::value_ptr(_modelMatrix));
 }
 
 //------------------------------------------------------------------------------
@@ -79,11 +87,11 @@ void GraphicEngine::render(GLFWwindow* window)
     // on dessine dans un framebuffer personnel
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    _framebuffers[0]->bind();
+    _framebuffers[frameBuffersCor["toBlit"]]->bind();
     glEnable(GL_DEPTH_TEST);
-    _framebuffers[0]->clear();
+    _framebuffers[frameBuffersCor["toBlit"]]->clear();
 
-    _shaders[0]->use();
+    _shaders[pipelinesCor["default"]]->use();
     for (auto& pair : _meshes){
         for(auto& mesh : pair.second){
             if(pair.first == globals::entitiesCor["screen"]){
@@ -96,11 +104,11 @@ void GraphicEngine::render(GLFWwindow* window)
     }
 
     // on dessine sur le framebuffer par défaut
-    _framebuffers[0]->unbind();
+    _framebuffers[frameBuffersCor["toBlit"]]->unbind();
     glClearColor(0.2f, 0.3f, 0.9f, 1.0f);
     glDisable(GL_DEPTH_TEST);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-    _shaders[1]->use();
+    _shaders[pipelinesCor["screen"]]->use();
     glBindTexture(GL_TEXTURE_2D, _framebuffers[0]->getTexture());
     _meshes[globals::entitiesCor["screen"]][0]->arm_for_drawing();
     _meshes[globals::entitiesCor["screen"]][0]->render();
